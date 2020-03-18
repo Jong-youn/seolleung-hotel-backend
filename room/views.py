7ed42e7c25a07e2e128f59af25023b3a071429f5
kay <k904808@gmail.com>
2020-03-18 11:11:58 +0900

    Refactor room/views.py and tests.py Alter room/views.py and tests.py

--- room/views.py
+++ room/views.py
@@ -10,41 +10,57 @@ class DetailView(View):
         room_data = (
             Room
             .objects
-            .select_related('room_type')
+            .select_related('branch')
+            .prefetch_related(
+                'roomimage_set',
+                'view_set',
+                'comfort_set',
+                'bathroom_set',
+                'entertainment_set',
+                'bedding_set',
+                'furnishing_set',
+                'fnbservice_set',
+                'laundry_set',
+                'safety_set'
+            )
             .get(id = room_id)
         )
+
         details = {
-            'images' : list(
-                room_data
-                .room_type
-                .roomimage_set
-                .values_list('image', flat = True)
-            ),
+            'images' : list(room_data.roomimage_set.values_list('image', flat = True)),
 
             'iconic_info' : {
-                'free_parking'         : room_data.room_type.iconic_info.free_parking,
-                'free_wifi'            : room_data.room_type.iconic_info.free_wifi,
-                'non_smoking'          : room_data.room_type.iconic_info.non_smoking,
-                'room_square_meter'    : room_data.room_type.iconic_info.room_square_meter,
-                'bed_type'             : room_data.room_type.iconic_info.bed_type.values('name')[0]['name'],
-                'room_square_meter_py' : room_data.room_type.iconic_info.room_square_meter_py,
-                'tv'                   : room_data.room_type.iconic_info.tv,
-                'refrigerator'         : room_data.room_type.iconic_info.refrigerator
+                'free_parking'         : room_data.free_parking,
+                'free_wifi'            : room_data.free_wifi,
+                'non_smoking'          : room_data.non_smoking,
+                'room_square_meter'    : room_data.room_square_meter,
+                'room_square_meter_py' : room_data.room_square_meter_py,
+                'tv'                   : room_data.tv,
+                'refrigerator'         : room_data.refrigerator,
+                'bed_type'             : list(room_data.bed.values_list('name', flat = True)),
             },
 
             'room_info' : {
-                'view'          : room_data.room_type.room_information.view.name,
-                'comfort'       : room_data.room_type.room_information.comfort.name,
-                'bathroom'      : room_data.room_type.room_information.bathroom.name,
-                'entertainment' : room_data.room_type.room_information.entertainment.name,
-                'bedding'       : room_data.room_type.room_information.bedding.name,
-                'furnishing'    : room_data.room_type.room_information.furnishing.name,
-                'fnb_service'   : room_data.room_type.room_information.fnb_service.name,
-                'laundry'       : room_data.room_type.room_information.laundry.name,
-                'safety'        : room_data.room_type.room_information.safety.name
+                'view'          : list(room_data.view_set.values_list('name',flat = True)),
+                'comfort'       : list(room_data.comfort_set.values_list('name',flat = True)),
+                'bathroom'      : list(room_data.bathroom_set.values_list('name',flat = True)),
+                'entertainment' : list(room_data.entertainment_set.values_list('name',flat = True)),
+                'bedding'       : list(room_data.bedding_set.values_list('name',flat = True)),
+                'furnishing'    : list(room_data.furnishing_set.values_list('name',flat = True)),
+                'fnbservice'    : list(room_data.fnbservice_set.values_list('name',flat = True)),
+                'laundry'       : list(room_data.laundry_set.values_list('name',flat = True)),
+                'safety'        : list(room_data.safety_set.values_list('name',flat = True)),
             },
 
-            'facility_info' : room_data.branch.facility.name
-}
+            'facility_info' : {
+                'neighbour' : list(room_data.branch.neighbour_set.values_list('name', flat = True)),
+                'dining'    : list(room_data.branch.dining_set.values_list('name', flat = True)),
+                'transport' : list(room_data.branch.transport_set.values_list('name', flat = True)),
+                'service'   : list(room_data.branch.service_set.values_list('name', flat = True)),
+                'entry'     : list(room_data.branch.entry_set.values_list('name', flat = True)),
+                'facility'  : list(room_data.branch.facility_set.values_list('name', flat = True)),
+                'language'  : list(room_data.branch.language_set.values_list('name', flat = True)),
+            }
+        }
 
         return JsonResponse({'details' : details}, status = 200)
